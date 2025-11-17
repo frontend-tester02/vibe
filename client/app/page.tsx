fc1a79d95a90b0fbdb080b968f7d69bc229294cc
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-11-17 18:25:51 +0900

    Setup tRPC

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,20 @@
-const Page = () => {
-	return <div>Hello world</div>
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { trpc, getQueryClient } from '../trpc/server'
+import { Suspense } from 'react'
+import ClientPage from './client'
+const Page = async () => {
+	const queryClient = getQueryClient()
+	void queryClient.prefetchQuery(
+		trpc.createAI.queryOptions({ text: 'Shokhrukh' })
+	)
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<Suspense fallback={<p>Loading...</p>}>
+				<ClientPage />
+			</Suspense>
+		</HydrationBoundary>
+	)
 }
 
 export default Page
